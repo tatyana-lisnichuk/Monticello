b729ae9e87683b920b58d4ce5b51c55f0d190c90
Tatyana <tanyshechka14121986@gmail.com>
2020-12-27 14:01:21 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,210 @@
+"use strict";
+$(document).ready(function () {
+  // включаем слайдер, используя нужный класс, указанный в html
+  $('.gallery__slider').slick({
+  slidesToShow:3,
+    slidesToScroll:1,
+    infinite:true,//бесконечный слайдер
+  });
+  $('.hero-slider').slick({
+  slidesToShow:1,
+    slidesToScroll:1,
+    infinite:true,//бесконечный слайдер
+  });
+  
+});
+//меню-бургер
+$('.menu__burger').click(function (event) {
+    $('.menu__burger,.nav').toggleClass('active');
+   
+
+});
+
+//карта
+
+function initMap() {}
+
+initMap = function() {
+  const myLatLng = { lat: 50.50827190529146,  lng: 30.498301659794745 };
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: myLatLng,
+    disableDefaultUI: true,
+    mapTypeControl: true,
+    styles: [
+      {
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#f5f5f5"
+          }
+        ]
+      },
+      {
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#616161"
+          }
+        ]
+      },
+      {
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#f5f5f5"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#bdbdbd"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#eeeeee"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#e5e5e5"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#9e9e9e"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#dadada"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#616161"
+          }
+        ]
+      },
+      {
+        featureType: "road.local",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#9e9e9e"
+          }
+        ]
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#e5e5e5"
+          }
+        ]
+      },
+      {
+        featureType: "transit.station",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#eeeeee"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#c9c9c9"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#9e9e9e"
+          }
+        ]
+      }
+    ]
+  });
+
+  const image = new google.maps.MarkerImage(
+    "../images/marker.png",
+    new google.maps.Size(130, 130),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(22, 32)
+  );
+
+  const marker = new google.maps.Marker({
+    position: myLatLng,
+    icon: image,
+    map: map,
+    title: "Beetroot Academy"
+  });
+};
